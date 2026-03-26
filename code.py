483ee638bc93960aa5caf4c78699c505a70c28d9
hsiu9517 <hsiu9517@gmail.com>
2026-03-26 23:10:44 +0800

    first commit

---
+++ code.py
@@ -0,0 +1,17 @@
+import requests
+import json
+import csv
+
+url = 'https://api.thecatapi.com/v1/images/search?limit=5'
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    cat_data = response.json()
+
+    # store as json format 
+    with open('cats_data.json', 'w', encoding='utf-8') as json_file:
+        json.dump(cat_data, json_file, ensure_ascii=False, indent=4)
+
+else:
+    print(f"failed：{response.status_code}")
