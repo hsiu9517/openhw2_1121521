ac61dbf563cd1893331ac252e5fca601f470dea8
Yi-Hsiu Liu <hsiu9517@gmail.com>
2026-04-03 01:54:07 +0800

    Update code.py

--- code.py
+++ code.py
@@ -2,16 +2,19 @@ import requests
 import json
 import csv
 
+# 設定連接的api
 url = 'https://api.thecatapi.com/v1/images/search'
 
+# 透過api用GET抓取資料
 response = requests.get(url)
 
+# 判斷是否GET成功，status_code == 200表示成功
 if response.status_code == 200:
-    cat_data = response.json()
+    cat_data = response.json()  # 儲存GET到的資料，將伺服器回傳的json資料轉成list
 
-    # store as json format 
+    # 回傳的資料儲存為json檔案
     with open('cats_data.json', 'w', encoding='utf-8') as json_file:
         json.dump(cat_data, json_file, ensure_ascii=False, indent=4)
-
+# 如果GET失敗
 else:
-    print(f"failed：{response.status_code}")
+    print(f"failed：{response.status_code}") # 輸出錯誤代碼
